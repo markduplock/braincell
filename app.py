788fecb56a4caaf0127bf87ea043e2b956d086bf
Mark <149008372+markduplock@users.noreply.github.com>
2026-05-29 20:19:57 +0100

    Placeholder pages and routing

--- app.py
+++ app.py
@@ -6,3 +6,18 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/pages")
+def pages():
+    return render_template("pages.html")
+
+
+@app.route("/topics")
+def topics():
+    return render_template("topics.html")
+
+
+@app.route("/recent")
+def recent():
+    return render_template("recent.html")
